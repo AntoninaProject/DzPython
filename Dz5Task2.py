79f320e4e582d7894389303b6400025002a9a339
Antonina Petrova <ant2466@yandex.ru>
2023-05-13 00:54:08 +0300

    Dz5Task2

---
+++ Dz5Task2.py
@@ -0,0 +1,18 @@
+# Дан список случайных чисел. Создайте список, в который попадают числа,
+# описывающие возрастающую последовательность. Порядок элементов менять нельзя.
+# [1, 5, 2, 3, 4, 6, 1, 7] =>[1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
+import random
+
+numbers = [random.randint(1, 10) for i in range (10)]
+print(numbers)
+
+result = [0]
+    
+while len(result) < 2:
+        index = random.randint(0, 9)  # случайный индекс, с которого начнем возрастающую последовательность
+        result = [numbers[index]]
+        for i in numbers[index:]:
+            if i > result[len(result)-1]:
+                result.append(i)    
+        
+print(result)    
